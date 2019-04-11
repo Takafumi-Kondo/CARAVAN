77c5afd9e830f70bc51a810053c2e1bb60cf1695
kondo <aa@kondo.local>
2019-04-10 19:10:15 -0700

    [Finish] 4-4

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -2,18 +2,22 @@ class BlogsController < ApplicationController
   def show
   end
 
+#topページに対応するアクションindex.投稿されたのを取得する
   def index
+    @blogs = Blog.all #取得したのをインスタンス変数としてビューへ
   end
 
   def new
   	@blog = Blog.new #form_forを使うのでインスタンス変数。Blogモデルから新規登録用にBlogインスタンス作成
   end
-#投稿保存createアク追加
-  def create
-  	
-  end
 
   def edit
+  end
+#投稿保存createアク追加
+  def create
+  	blog = Blog.new(blog_params)
+    blog.save
+    redirect_to blogs_path
   end
 
   private #createアクにフォームから送られてきた投稿を保存する
